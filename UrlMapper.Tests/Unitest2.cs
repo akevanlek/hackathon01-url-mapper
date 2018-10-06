2f86e11913d7e9e0b96749f75bf805f548e9eac6
tatae <tachapong999.gmail.com>
2018-10-06 14:23:30 +0700

    Test Bug

---
+++ UrlMapper.Tests/Unitest2.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UrlMapper;
+using Xunit;
+
+namespace UrlMapper.Tests
+{
+    public class Unitest2
+    {
+
+        [Theory]
+        [InlineData("https://mana.com/linkto/{link-id}", "https://mana.com/linkto/A2752348", "{link-id}", "A2752348")]
+        [InlineData("http://google.com/?s={keyword}", "http://google.com/?s=A2752348", "{keyword}", "A2752348")]
+        [InlineData("http://google.com/?s={keycs1word}", "http://google.com/?s=A27v/52348", "{keycs1word}", "A27v/52348")]
+        [InlineData("https://mana.com/linkto/{link/*-id}", "https://mana.com/linkto/A2752348", "{link/*-id}", "A2752348")]
+        [InlineData("https://mana.com/linkto/{link0-id}", "https://mana.com/linkto/A27/*2348", "{link0-id}", "A27/*2348")]
+        [InlineData("http://google.com/?s={keyword}", "http://google.com/?s=A2752348", "{keyword}", "A2752348")]
+        [InlineData("http://google.com/?//s={keyword}", "http://google.com/?//s=A27523485666", "{keyword}", "A27523485666")]
+        [InlineData(@"http://google.com/?//s={keyrtktyktyjd[]rj4362#$#$%^%^%$word}", @"http://google.com/?//s=A275234*&%^&*(&*)($$$@@$$%$%_/85666", "{keyrtktyktyjd[]rj4362#$#$%^%^%$word}", "A275234*&%^&*(&*)($$$@@$$%$%_/85666")]
+
+
+        public void ManaLinkTo(string patternUrl, string tragetUrl, string key, string expectedResult)
+        {
+            var builder = new SimpleStringParameterBuilder();
+
+            var result = builder.Parse(patternUrl);
+            bool isMatch = result.IsMatched(tragetUrl);
+            Assert.True(isMatch);
+
+            var dic = new Dictionary<string, string>();
+            result.ExtractVariables(tragetUrl, dic);
+            Assert.True(dic.ContainsKey(key));
+            Assert.Equal(expectedResult, dic[key]);
+        }
+    }
+}
